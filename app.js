d98daed3f06f329ca90d3ebc5a288456658cd650
Ferran <ferran.docencia@gmail.com>
2025-06-19 09:29:23 +0200

    plantilla travels

--- app.js
+++ app.js
@@ -5,9 +5,30 @@ const app = express()
 process.loadEnvFile()
 const PORT = process.env.PORT
 
-app.get("/", (req, res) => {
-    res.send('OK')
+// Ruta de los recursos estáticos
+app.use(express.static(path.join(__dirname, "public")))
+
+// Vistas
+app.set("view engine", "ejs")
+app.set("views", "./views")
+
+// Importar los datos
+const jsonData = require("./data/travels.json")
+
+jsonData.forEach( travel => {
+    app.get(`${travel.ruta}`, (req, res) => {
+        res.render("travels", {
+            "lugar" : `${travel.lugar}`, 
+            "descripcion" : `${travel.descripcion}`,
+            "precio" : `${travel.precio}`, 
+            "nombre" : `${travel.nombre}`, 
+            "img" : `${travel.img}`,
+            "travels" : jsonData
+        })
+    })
 })
+
+
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en http://localhost:${PORT}`);
